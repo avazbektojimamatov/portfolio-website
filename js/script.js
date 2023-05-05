847e13a47aea7db3bd65812b84523259381e8bd1
Avazbek Tojimamatov <tojimamatov.avazbek2002@gmail.com>
2023-05-05 11:19:30 +0500

    form submit to telegram

--- js/script.js
+++ js/script.js
@@ -60,3 +60,51 @@ const typed = new Typed('.multiple-text', {
   backDelay: 1000,
   loop: true
 })
+
+// input mobile number
+
+function formatPhoneNumber() {
+  var input = document.getElementById('phone-input');
+  var phoneNumber = input.value.replace(/\D/g, '');
+  var formattedPhoneNumber = '';
+  var phoneLength = phoneNumber.length;
+
+  if (phoneLength > 0) {
+    formattedPhoneNumber = '+' + phoneNumber.substring(0, 3);
+  }
+  if (phoneLength > 3) {
+    formattedPhoneNumber += ' ' + phoneNumber.substring(3, 5);
+  }
+  if (phoneLength > 5) {
+    formattedPhoneNumber += ' ' + phoneNumber.substring(5, 8);
+  }
+  if (phoneLength > 8) {
+    formattedPhoneNumber += ' ' + phoneNumber.substring(8, 10);
+  }
+  if (phoneLength > 10) {
+    formattedPhoneNumber += ' ' + phoneNumber.substring(10, 12);
+  }
+
+  input.value = formattedPhoneNumber;
+}
+
+// Form submit to telegram
+function sendMessage() {
+  var name = document.querySelector('input[type="text"][placeholder="Full Name"]').value;
+  var email = document.querySelector('input[type="email"][placeholder="Email Address"]').value;
+  var phone = document.getElementById('phone-input').value;
+  var message = document.querySelector('textarea[name="message"]').value;
+  var chatId = '-897613182';
+  var token = '6040569885:AAEpi3Td5XuIrOrrRcyf3nEHt7hFUroS0m0';
+  var text = `Name: ${name}%0AEmail: ${email}%0APhone: ${phone}%0AMessage: ${message}`;
+  var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${text}`;
+  axios.get(url)
+    .then(function (response) {
+      console.log(response);
+      alert('Message sent!');
+    })
+    .catch(function (error) {
+      console.log(error);
+      alert('Message failed to send.');
+    });
+}
